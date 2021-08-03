493e1e2368edee96e932edac7d3078b38df9bdb7
ShangzhenWei <2541221w@student.gla.ac.uk>
2021-08-04 00:34:20 +0100

    About Page modification

--- rango/views.py
+++ rango/views.py
@@ -31,8 +31,6 @@ def index(request):
 
 def about(request):
     context_dict = {}
-    visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
     #if request.session.test_cookie_worked():
     #    print("TEST COOKIE WORKED!")
     #    request.session.delete_test_cookie()
