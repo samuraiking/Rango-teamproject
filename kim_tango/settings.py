3355bcfb1d023b15e83a43eb8cde2934d735df0b
unknown <2531986c@student.gla.ac.uk>
2021-08-04 04:34:01 +0100

    interface update

--- kim_tango/settings.py
+++ kim_tango/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango'
+    'rango',
+    'registration'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
+# If True, users can register.
+REGISTRATION_OPEN = True
+# If True, the user will be automatically logged in after registering.
+REGISTRATION_AUTO_LOGIN = True
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL = 'rango:index'
+# The page users are directed to if they are not logged in.
+# This was set in a previous chapter. The registration package uses this, too.
+LOGIN_URL = 'auth_login'
--- kim_tango/urls.py
+++ kim_tango/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('rango/',include('rango.urls')),
 #    path('static/',include(STATIC_URL)),
     path('admin/', admin.site.urls),
+    path('accounts/',include('registration.backends.simple.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
