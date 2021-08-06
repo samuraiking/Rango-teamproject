4e55076927f287eae4b1913aeb91b4458006b3d6
Shengyu Jin <jinshengyu1996@gmail.com>
2021-08-06 16:11:38 +0100

    research fixed

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,7 @@ from rango.forms import UserForm,UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from rango.bing_search import run_query
 
 
 def index(request):
@@ -116,3 +117,12 @@ def visitor_cookie_handler(request):
         request.session['last_visit'] = last_visit_cookie
     
     request.session['visits'] = visits
+
+def search(request):
+    result_list = []
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+        if query:# Run our Bing function to get the results list!
+            result_list = run_query(query)
+    return render(request, 'rango/search.html', {'result_list': result_list})
+
